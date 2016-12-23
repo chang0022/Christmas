9a0501df9383034a2a5925b9320eeee6abeecea8
chang0022 <chang.0022@163.com>
2016-12-23 17:26:13 +0800

    v1

---
+++ script/DATA.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Neo on 2016/12/23.
+ */
+var User = [
+    {
+        phone: '15670007176',
+        name: '老常',
+        awards: 1
+    },
+    {
+        phone: '13193598797',
+        name: '媳妇',
+        awards: 2,
+        image: 'image/2.jpg',
+        desc: '电动牙刷一只',
+        result: '恭喜你获得二等奖！'
+    },
+    {
+        phone: '15067134475',
+        name: '围观群众',
+        awards: 5
+    }
+];
+
+var Thanks = ['谢谢参与~','抱歉，空的', 'Merry Christmas', '圣诞快乐'];
